36e57f81e1e60179daba6df421cbf742fa608722
ananya sunkari <ananyas.sunkari@gmail.com>
2024-10-27 20:05:10 +0000

    Update CameraControl.cs

--- Assets/Scripts/Controller/CameraControl.cs
+++ Assets/Scripts/Controller/CameraControl.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//test pull request
 public class CameraControl : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
