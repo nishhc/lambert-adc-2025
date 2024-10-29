2376d63d8b65943c17bb221f70a648ba4807286e
nishhc <nishchai@outlook.com>
2024-10-28 21:23:12 -0400

    Added .csv, fixed syntax issues with csv parser

--- Assets/Scripts/CSV_Parser.cs
+++ Assets/Scripts/CSV_Parser.cs
@@ -1,50 +1,72 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CSV_Parser : MonoBehaviour
 {
-    List<Dictionary<string, int>> flightData = new List<Dictionary<string, int>>();
-    void Awake()
+    [SerializeField] private TextAsset csvFile;
+    private List<Dictionary<string, double>> flightData = new List<Dictionary<string, double>>();
+
+    private void Awake()
     {
-        flightData = GetDataFromCSV('csv_data.csv')
+        if (csvFile != null)
+        {
+            flightData = GetDataFromCSV(csvFile);
+            PrintFlightData();
+        }
+        else
+        {
+            Debug.LogError("No CSV file assigned in the inspector.");
+        }
     }
 
-    //Returns a list of dictionaries that stores all flight data
-    //Each entry of the the list is a dictionary that stores all information about the flight at at a given time
-    List<Dictionary<string, int>> GetDataFromCSV(filename)
+    private List<Dictionary<string, double>> GetDataFromCSV(TextAsset csvData)
     {
-        var dataFile = Resources.Load<TextAsset>('csv_data.csv'); //Loads data from file
-        var dataLines = dataset.text.Split('\n'); //Returns list where each entry is a row of data
-        dataLines.removeAt(0) //Removes header information from data
-        var data = new List<Dictionary<string, int>>() //Creates empty list of data
-        foreach (String line in dataLines)
+        var dataLines = csvData.text.Split('\n');
+        if (dataLines.Length <= 1) return null;
+        var headers = dataLines[0].Split(',');
+        var data = new List<Dictionary<string, double>>();
+
+        for (int i = 1; i < dataLines.Length; i++)
         {
+            var line = dataLines[i].Trim();
+            if (string.IsNullOrEmpty(line)) continue;
+
             var values = line.Split(',');
-            var data_dict = new Dictionary<string, int> {
-                {'MISSION_ELAPSED_TIME' : values[0]},
-                { 'ROTATION_X' : values[1]},
-                { 'ROTATION_Y' : values[2]},
-                { 'ROTATION_Z' : values[3]},
-                { 'VELOCITY_X' : values[4]},
-                { 'VELOCITY_Y' : values[5]},
-                { 'VELOCITY_Z' : values[6]},
-                { 'MASS' : values[7]},
-                { 'EARTH_ROTATION_X' : values[8]},
-                { 'EARTH_ROTATION_Y' : values[9]},
-                { 'EARTH_ROTATION_X' : values[10]},
-                { 'EARTH_VELOCITY_X' : values[11]},
-                { 'EARTH_VELOCITY_Y' : values[12]},
-                { 'EARTH_VELOCITY_Z' : values[13]},
-                { 'MOON_ROTATION_X' : values[14]},
-                { 'MOON_ROTATION_Y' : values[15]},
-                { 'MOON_ROTATION_X' : values[16]},
-                { 'MOON_VELOCITY_X' : values[17]},
-                { 'MOON_VELOCITY_Y' : values[18]},
-                { 'MOON_VELOCITY_Z' : values[19]},
-            };
-    data.Add(data_line);
+
+            var dataDict = new Dictionary<string, double>();
+            for (int j = 0; j < headers.Length && j < values.Length; j++)
+            {
+                if (double.TryParse(values[j], out double doubleValue))
+                {
+                    dataDict[headers[j]] = doubleValue;
+                }
+
+                /* A lot of these are empty, so this just floods the console with warnings. not exactly sure what to do with this.
+                most likely using machine learning later for data cleaning
+                that'll be done by proj supervisors, not reg members unless experience shown 
+                
+                else 
+                {
+                    Debug.LogWarning($"Unable to parse '{values[j]}' as double in row {i + 1}, column '{headers[j]}'.");
+                }
+                */
+            }
+
+            data.Add(dataDict);
         }
-return data;
+        return data;
+    }
+
+    private void PrintFlightData()
+    {
+        foreach (var row in flightData)
+        {
+            string rowString = "";
+            foreach (var kvp in row)
+            {
+                rowString += $"{kvp.Key}: {kvp.Value}, ";
+            }
+            Debug.Log(rowString);
+        }
     }
 }
