40003bab4cf20fac03e288a0fc4ece4f79dfcc97
nishhc <nishchai@outlook.com>
2024-11-03 15:57:12 -0500

    Resolved Merge Conflicts

--- Assets/Scripts/CSV_Parser.cs
+++ Assets/Scripts/CSV_Parser.cs
@@ -8,44 +8,6 @@ public class CSV_Parser : MonoBehaviour
 
     private void Awake()
     {
-<<<<<<< HEAD
-        flightData = GetDataFromCSV("csv_data.csv");
-    }
-
-    //Returns a list of dictionaries that stores all flight data
-    //Each entry of the the list is a dictionary that stores all information about the flight at at a given time
-    List<Dictionary<string, int>> GetDataFromCSV(string filename)
-    {
-        var dataFile = Resources.Load<TextAsset>(filename); //Loads data from file
-        var dataLines = dataFile.text.Split("\n"); //Returns list where each entry is a row of data
-        var data = new List<Dictionary<string, int>>(); //Creates empty list of data
-        foreach (string line in dataLines)
-        {
-            var values = line.Split(",");
-            var data_dict = new Dictionary<string, int> {
-                { "MISSION_ELAPSED_TIME", int.Parse(values[0])},
-                { "ROTATION_X", int.Parse(values[1])},
-                { "ROTATION_Y", int.Parse(values[2])},
-                { "ROTATION_Z", int.Parse(values[3])},
-                { "VELOCITY_X", int.Parse(values[4])},
-                { "VELOCITY_Y", int.Parse(values[5])},
-                { "VELOCITY_Z", int.Parse(values[6])},
-                { "MASS", int.Parse(values[7])},
-                { "EARTH_ROTATION_X", int.Parse(values[8])},
-                { "EARTH_ROTATION_Y", int.Parse(values[9])},
-                { "EARTH_ROTATION_X", int.Parse(values[10])},
-                { "EARTH_VELOCITY_X", int.Parse(values[11])},
-                { "EARTH_VELOCITY_Y", int.Parse(values[12])},
-                { "EARTH_VELOCITY_Z", int.Parse(values[13])},
-                { "MOON_ROTATION_X", int.Parse(values[14])},
-                { "MOON_ROTATION_Y", int.Parse(values[15])},
-                { "MOON_ROTATION_X", int.Parse(values[16])},
-                { "MOON_VELOCITY_X", int.Parse(values[17])},
-                { "MOON_VELOCITY_Y", int.Parse(values[18])},
-                { "MOON_VELOCITY_Z", int.Parse(values[19])},
-            };
-    data.Add(data_dict);
-=======
         if (csvFile != null)
         {
             flightData = GetDataFromCSV(csvFile);
@@ -105,7 +67,6 @@ public class CSV_Parser : MonoBehaviour
                 rowString += $"{kvp.Key}: {kvp.Value}, ";
             }
             Debug.Log(rowString);
->>>>>>> 2376d63d8b65943c17bb221f70a648ba4807286e
         }
     }
 }
