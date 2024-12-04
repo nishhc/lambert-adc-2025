ac8d1fec624dfe05db47c34456415501582e6226
donut <thegyageedonut@gmail.com>
2024-12-03 23:50:15 -0500

    Made minimap pointers maintain a constant size despite zoom

--- Assets/Scripts/UI/PreserveMinimapIcons.cs
+++ Assets/Scripts/UI/PreserveMinimapIcons.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class PreserveMinimapIcons : MonoBehaviour
 {
-    //sigma
+    [SerializeField] private Camera minimapCam;
+    private Vector3 initialScale;
+    private float initialLineWidth;
+
+    void Start()
+    {
+        initialScale = transform.localScale;
+
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        if (lineRenderer != null)
+        {
+            initialLineWidth = lineRenderer.startWidth;
+        }
+    }
+
+    void Update()
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        if (lineRenderer != null)
+        {
+            lineRenderer.startWidth = initialLineWidth / (30f / minimapCam.orthographicSize);
+            lineRenderer.endWidth = initialLineWidth / (30f / minimapCam.orthographicSize);
+        }
+        else
+        {
+            transform.localScale = initialScale / (30f / minimapCam.orthographicSize);
+        }
+    }
 }
